83ff133b80fbdbcdddc19dbb64802a5a416463d5
Pavitra16 <138608174+Pavitra16@users.noreply.github.com>
2025-10-04 01:38:06 +0530

    Time: 221 ms (16.80%) | Memory: 45.2 MB (16.09%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,29 +1,28 @@
 class Solution {
-    public boolean exist(char[][] b, String word) {
-        int n=b.length;
-        int m=b[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(b[i][j]==word.charAt(0)){
-                    boolean ans=solve(b,word,i,j,0);
+    public boolean exist(char[][] board, String word) {
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]==word.charAt(0)){
+                    boolean ans=solve(board,word,0,i,j,board.length,board[0].length);
                     if(ans) return true;
                 }
             }
         }
         return false;
-    }
-    public boolean solve(char a[][], String s, int cr, int cc,int ind){
-        if(ind==s.length()) return true;
-        if(cr<0 || cc<0 || cr>=a.length || cc>=a[0].length ||s.charAt(ind)!=a[cr][cc]) return false;
-        a[cr][cc]='*';
-        int r[]={0,-1,0,1};
-        int c[]={-1,0,1,0};
-        for(int i=0;i<r.length;i++){
-            boolean ans=solve(a,s,cr+r[i],cc+c[i],ind+1);
-            if(ans) return true;
 
-        }
-        a[cr][cc]=s.charAt(ind);
-        return false;
+    }
+    public boolean solve(char grid[][] ,String word, int  ind,int cr, int cc, int er, int ec){
+    if(ind==word.length()) return true;
+    if(cr>=er || cr<0 || cc<0 || cc>=ec || ind>word.length() || word.charAt(ind)!=grid[cr][cc]) return false;
+    grid[cr][cc]='*';
+    int r[]={0,-1,0,1};
+    int c[]={-1,0,1,0};
+    for(int i=0;i<r.length;i++){
+        boolean ans=solve(grid,word,ind+1,cr+r[i],cc+c[i],er,ec);
+        if(ans) return true;
+    }
+    grid[cr][cc]=word.charAt(ind);
+    return false;
+
     }
 }
