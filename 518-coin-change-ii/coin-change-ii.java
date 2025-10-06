985c0debcaf6a97bd9355486bf32ef3d87622e8b
Pavitra16 <138608174+Pavitra16@users.noreply.github.com>
2025-10-06 11:03:13 +0530

    Time: 25 ms (10.15%) | Memory: 54.9 MB (5.00%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        // int dp[][] = new int[amount+1][coins.length];
-        // for(int a[]:dp)Arrays.fill(a,-1);
-        // return solve1(amount,coins,0,dp);
-        return solve2(amount,coins);
+         int dp[][] = new int[amount+1][coins.length];
+         for(int a[]:dp)Arrays.fill(a,-1);
+         return solve1(amount,coins,0,dp);
+       // return solve2(amount,coins);
     }
     public int solve1(int amt, int[] coins, int i,int[][] dp){
         if(amt==0) return 1;
-        if(i>=coins.length || coins[i]>amt) return 0;
+        if(i>=coins.length ) return 0;
         if(dp[amt][i]!=-1) return dp[amt][i];
         int inc=0;
         int exc=0;
-        inc=solve1(amt-coins[i],coins,i,dp);
+       if(amt>=coins[i]) inc=solve1(amt-coins[i],coins,i,dp);
         exc=solve1(amt,coins,i+1,dp);
         return dp[amt][i]=inc+exc;
 
