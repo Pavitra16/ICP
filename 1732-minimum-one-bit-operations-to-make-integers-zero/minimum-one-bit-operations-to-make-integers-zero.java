feba18d567b1e397fdb755cea39ef6a5d51a8f2f
Pavitra16 <138608174+Pavitra16@users.noreply.github.com>
2025-11-08 21:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (9.00%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumOneBitOperations(int n) {
+       return helper(n);
+    }
+
+    private int helper(int n) {
+        if (n == 0) return 0;
+        int k = 0;
+        while ((1 << (k + 1)) <= n) k++;
+        return (1 << (k + 1)) - 1 - helper(n ^ (1 << k));   
+    }
+}
