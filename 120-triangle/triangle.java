93aa3c0ba2e6fe33396c9525ca2594afe11b2c8e
Pavitra16 <138608174+Pavitra16@users.noreply.github.com>
2025-11-21 19:35:24 +0530

    Time: 1 ms (99.41%) | Memory: 44.9 MB (99.50%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,15 +2,14 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
         int dp[][]=new int[n][n];
-        for(int a[]:dp)Arrays.fill(a,Integer.MAX_VALUE);
+        for(int a[]:dp)Arrays.fill(a,Integer.MAX_VALUE) ;
         return solve(triangle,0,0,dp);
-
     }
-    public int solve(List<List<Integer>>lst,int i,int j, int dp[][]){
-        if(j>=lst.size()) return 0;
-        if(dp[j][i]!=Integer.MAX_VALUE) return dp[j][i];
-        int same=solve(lst,i,j+1,dp);
-        int adj=solve(lst,i+1,j+1,dp);
-        return dp[j][i]=lst.get(j).get(i)+Math.min(same,adj);
+    public int solve(List<List<Integer>>lst, int i, int j,int dp[][]){
+        if(i>=lst.size() || j>=lst.get(i).size()) return 0;
+        if(dp[i][j]!=Integer.MAX_VALUE) return dp[i][j];
+        int ind=lst.get(i).get(j)+solve(lst,i+1,j,dp);
+        int ind1=lst.get(i).get(j)+solve(lst,i+1,j+1,dp);
+        return dp[i][j]=Math.min(ind,ind1);
     }
 }
