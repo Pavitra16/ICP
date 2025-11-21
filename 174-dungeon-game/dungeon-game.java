40292da2969cda93ed4cfda4f237b5272230ddb0
Pavitra16 <138608174+Pavitra16@users.noreply.github.com>
2025-11-21 21:29:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (58.41%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -3,13 +3,13 @@ class Solution {
         int n=dungeon.length;
         int m=dungeon[0].length;
         int dp[][]=new int[n][m];
-        for(int a[]:dp) Arrays.fill(a,Integer.MAX_VALUE);
+        for(int a[]:dp) Arrays.fill(a,-1);
         return solve(dungeon,0,0,dp);
     }
     public int solve(int a[][], int i, int j, int dp[][]){
         if(i<0 || j<0 || i>=a.length || j>=a[0].length) return Integer.MAX_VALUE;
         if(i==a.length-1 && j==a[0].length-1) return Math.max(1,1-a[i][j]);
-        if(dp[i][j]!=Integer.MAX_VALUE)return dp[i][j];
+        if(dp[i][j]!=-1)return dp[i][j];
         int down=solve(a,i+1,j,dp);
         int right=solve(a,i,j+1,dp);
         int ans=Math.min(down,right);
